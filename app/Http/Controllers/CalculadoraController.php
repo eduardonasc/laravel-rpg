97f98cb42ac0360dd5eaadcbed44cb24839f519d
Eduardo Nascimento <eduardo.nascimento@mazzafc.tech>
2022-01-18 21:47:15 -0300

    first commit

---
+++ app/Http/Controllers/CalculadoraController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalculadoraController extends Controller
+{
+    public function calculadora()
+    {
+        return view('calculadora');
+    }
+
+    public function soma(Request $request)
+    {
+        // Pegar variáveis do formulário
+        $num1 = $request->num1;
+        $num2 = $request->num2;
+
+        // Calcular soma
+        $result = $num1 + $num2;
+
+        // Mostrar página para o usuário
+        return view('resultado', compact('result'));
+    }
+}
---
+++ resources/views/calculadora.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calculadora</title>
+</head>
+<body>
+    <form action="/soma" method="POST">
+        @csrf
+        <input type="number" name="num1" placeholder="Número 1">
+        <input type="number" name="num2" placeholder="Número 2">
+        <button type="submit">Somar</button>
+    </form>
+</body>
+</html>
